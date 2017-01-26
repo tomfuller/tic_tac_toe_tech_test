01894ad7bf82b4bf68979c4f4b6bba0de59e422a
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-26 13:16:49 +0000

    squares taken method added, new design is emerging, need a square class

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,39 @@
 #responsibility: keep track of the board
 class Board
 
-  attr_reader :squares
+  attr_reader :squares, :x_squares, :o_squares
 
   def initialize
     @squares = [[1,2,3],[4,5,6],[7,8,9]]
+    @x_squares = []
+    @o_squares = []
+  end
+
+  def mark_square(mark, square)
+    add_mark_to_square(mark, square)
+    remove_taken_square_from_sqaures(square)
+  end
+
+  def square_taken?(square)
+    return false if squares.flatten.include?(square)
+    true
+  end
+
+  private
+
+  def remove_taken_square_from_sqaures(square)
+    squares.each do |squares|
+      squares.reject! { |s| s == square }
+    end
+  end
+
+  def add_mark_to_square(mark, square)
+    x_squares.push(square) if mark == 'X'
+    o_squares.push(square) if mark == 'O'
+  end
+
+  def squares_include_square?(square)
+
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,6 +10,43 @@ describe Board do
       expect(board.squares).to eq([[1,2,3],[4,5,6],[7,8,9]])
     end
 
+    it "should be initialized with an empty array for X's moves" do
+      expect(board.x_squares).to eq []
+    end
+
+    it "should be initialized with an empty array for O's moves" do
+      expect(board.o_squares).to eq []
+    end
+
+  end
+
+  context 'recieveing a move' do
+
+    it 'should be able to take a mark X and a square and place mark in square' do
+      board.mark_square('X', 5)
+      expect(board.x_squares).to eq [5]
+      expect(board.squares).to eq([[1,2,3],[4,6],[7,8,9]])
+    end
+
+    it 'should be able to take a mark O and a square and place mark in square' do
+      board.mark_square('O', 5)
+      expect(board.o_squares).to eq [5]
+      expect(board.squares).to eq([[1,2,3],[4,6],[7,8,9]])
+    end
+
+  end
+
+  context 'check if square is taken' do
+
+    it 'should check if square is already taken return false' do
+      expect(board.square_taken?(6)).to eq false
+    end
+
+    it 'should check if square is already taken return true' do
+      board.mark_square('O', 6)
+      expect(board.square_taken?(6)).to eq true
+    end
+
   end
 
 end
